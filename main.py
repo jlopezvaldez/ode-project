6d5dc867d89599ca8b5026d028bf990bc4b1a060
Jonathan Lopez <59666853+jlopezvaldez@users.noreply.github.com>
2021-09-19 20:55:53 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import odeint
+
+#our ode model
+def model(y,t,k):
+    # k = 0.95 test condition
+    dydt = (k)/((((k/t)-k + 1)**2)*(t**2)) #derivitve of amdahls law
+    return dydt
+
+y0 = 1 #initial condition
+
+t = np.linspace(1,256) #time points
+
+k = .5
+y1 = odeint(model,y0,t,args=(k,))
+
+k = .75
+y2 = odeint(model,y0,t,args=(k,))
+
+k = .9
+y3 = odeint(model,y0,t,args=(k,))
+
+k = .95
+y4 = odeint(model,y0,t,args=(k,))
+
+# y = odeint(model,y0,t)
+
+plt.plot(t,y1, 'r-', linewidth = 2, label='k=0.50')
+plt.plot(t,y2, 'b--', linewidth = 2, label='k=0.75')
+plt.plot(t,y3, 'g:', linewidth = 2, label='k=0.90')
+plt.plot(t,y4, 'k+', linewidth = 2, label='k=0.95')
+
+plt.xticks(np.arange(min(t), max(t)+2, 15))
+
+plt.xlabel('Number of Processors n')
+plt.ylabel('Speedup Time')
+plt.legend()
+plt.show()
+
